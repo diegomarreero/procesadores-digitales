3cd638fc3a9191a61ed6b4c16548f6d981f55a31
diego <diegofabelo@gmail.com>
2025-02-20 12:49:15 +0100

    Add files via upload

---
+++ pr1/pr1-ej2.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED_BUILTIN 2
+#define DELAY 500
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("ON");
+    delay(DELAY);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("OFF");
+    delay(DELAY);
+}
